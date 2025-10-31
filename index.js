55e7eb1db731db654fd63c0860c997e41e4d688e
matthew <matthewapodaca24@gmail.com>
2025-10-30 19:15:41 -0700

    codes

---
+++ index.js
@@ -0,0 +1,26 @@
+const code1= 7+3;
+
+const code2= (100/2)-10;
+
+const code3= 40-(11%2); 
+console.log(code3);
+
+const message = "The vault has been secured. The combination is: ";
+console.log(message);
+
+codeA= (code1 + "-" + code2 + "-"+ code3);
+console.log(codeA);
+
+codeB= `${code1}-${code2}-${code3}`;
+console.log(codeB);
+
+
+
+
+
+
+
+
+
+
+
